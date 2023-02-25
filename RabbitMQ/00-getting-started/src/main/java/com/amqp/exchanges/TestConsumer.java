e429a59e366b4cf60dc31ded796052d44c31d950
juan.marulanda <juan.marulanda@ceiba.com.co>
2023-02-25 14:47:31 -0500

    RabbitMQ project added

---
+++ RabbitMQ/00-getting-started/src/main/java/com/amqp/exchanges/TestConsumer.java
@@ -0,0 +1,27 @@
+package com.amqp.exchanges;
+
+import com.amqp.basic.queue.CommonConfigs;
+import com.rabbitmq.client.*;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class TestConsumer {
+  public static void main(String[] args) throws IOException, TimeoutException {
+    ConnectionFactory factory = new ConnectionFactory();
+    Connection connection = factory.newConnection(CommonConfigs.AMQP_URL);
+    Channel channel = connection.createChannel();
+
+    DeliverCallback deliverCallback = (consumerTag, message) -> {
+      System.out.println(consumerTag);
+      System.out.println(new String(message.getBody(), "UTF-8"));
+    };
+
+    CancelCallback cancelCallback = consumerTag -> {
+      System.out.println(consumerTag);
+    };
+    channel.basicConsume("MobileQ", true, deliverCallback, cancelCallback);
+    channel.basicConsume("ACQ", true, deliverCallback, cancelCallback);
+    channel.basicConsume("LightQ", true, deliverCallback, cancelCallback);
+  }
+}
